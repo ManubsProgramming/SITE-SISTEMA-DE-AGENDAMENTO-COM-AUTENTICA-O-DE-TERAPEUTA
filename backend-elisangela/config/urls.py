93efa99e60df148f8f49b7e13614366b2b904511
ManubsProgramming <manuela.j.gamer1968@gmail.com>
2026-09-18 09:24:03 -0400

    Corrige rotas da anamnese

--- backend-elisangela/config/urls.py
+++ backend-elisangela/config/urls.py
@@ -5,6 +5,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("payments.urls")),
-    path("api/anamnesis/", include("anamnesis.urls")),
+    path("api/", include("anamnesis.urls")),
     path("api/dashboard/", include("dashboard.urls")),
 ]
